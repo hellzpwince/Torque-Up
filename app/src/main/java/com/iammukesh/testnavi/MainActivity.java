289221c80945e865f1891d1b4ef1a649a409deca
Mukesh Joshi <admin@iammukesh.com>
2016-03-01 20:49:50 +0530

    profile screen

--- app/src/main/java/com/iammukesh/testnavi/MainActivity.java
+++ app/src/main/java/com/iammukesh/testnavi/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import fragments.BatteryFragment;
@@ -73,6 +74,12 @@ public class MainActivity extends AppCompatActivity
         getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
     }
 
+    public void saveProfileOption(View view){
+        CheckBox socialcheck = (CheckBox) findViewById(R.id.socialcheck);
+        if(socialcheck.isChecked()==true){
+            Toast.makeText(getApplicationContext(),"Social is checked",Toast.LENGTH_SHORT).show();
+        }
+    }
     public void toast(View view){
         Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/fragments/profile_fragment.java
+++ app/src/main/java/fragments/profile_fragment.java
@@ -6,15 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.iammukesh.testnavi.MainActivity;
 import com.iammukesh.testnavi.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class profile_fragment extends Fragment {
-
-
+    public void saveProfileOption(View view){
+        CheckBox socialcheck = (CheckBox) getActivity().findViewById(R.id.socialcheck);
+        if(socialcheck.isChecked()==true){
+            Toast.makeText(getActivity().getApplicationContext(),"Social is checked",Toast.LENGTH_SHORT).show();
+        }
+    }
     public profile_fragment() {
         // Required empty public constructor
     }
@@ -25,6 +34,12 @@ public class profile_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_fragment, container, false);
+
     }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
 
 }
