24293843eba3138711bb22873ca6cec99229d89b
Mukesh Joshi <admin@iammukesh.com>
2016-05-15 21:42:32 +0530

    Removal of GPS service

--- app/src/main/java/com/iammukesh/testnavi/MainActivity.java
+++ app/src/main/java/com/iammukesh/testnavi/MainActivity.java
@@ -2,14 +2,11 @@ package com.iammukesh.testnavi;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,19 +17,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.List;
-
-import fragments.BatteryFragment;
 import fragments.BlankFragment;
-import fragments.GeoSettingFragement;
-import fragments.RamFragment;
 import fragments.SettingFragment;
 import fragments.profile_fragment;
 
@@ -46,15 +34,9 @@ public class MainActivity extends AppCompatActivity
     public String servicetest ="";
     public String background = "";
     public String screenKill ="";
-    public String useGPStest="";
-    public String optimizeAsILeavetest="";
-    public String leaveNetOntest="";
     public Integer progressBar;
     CheckBox backgroundCheck;
     CheckBox screenOffKill;
-    CheckBox leaveNetOn;
-    CheckBox useGPS;
-    CheckBox optimizeAsILeave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,15 +59,6 @@ public class MainActivity extends AppCompatActivity
         servicetest=sharedPreferences.getString("ServiceTest",null);
         background = sharedPreferences.getString("backgroundworking", null);
         screenKill=sharedPreferences.getString("screenoffkill", null);
-        useGPStest=sharedPreferences.getString("useGPS", null);
-        leaveNetOntest=sharedPreferences.getString("leaveNetOn", null);
-        optimizeAsILeavetest=sharedPreferences.getString("optimizeAsILeave", null);
-
-
-         Log.i("headerName",String.valueOf(game));
-         Log.i("headerName",String.valueOf(develop));
-         Log.i("headerName",String.valueOf(background));
-         Log.i("headerName",String.valueOf(progressBar));
        // Toast.makeText(MainActivity.this, headerProfileName, Toast.LENGTH_LONG).show();
         startService();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -97,13 +70,10 @@ public class MainActivity extends AppCompatActivity
     public void startService(){
     startService(new Intent(getBaseContext(),OptimizeService.class));
     }
-public void changewifi(View view){
+public void changewifi(View view) {
     WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
     wifiManager.setWifiEnabled(true);
     boolean wifiEnabled = wifiManager.isWifiEnabled();
-    Toast.makeText(MainActivity.this, wifiEnabled+ " " , Toast.LENGTH_SHORT).show();
-    Toast.makeText(MainActivity.this, servicetest, Toast.LENGTH_SHORT).show();
-
 }
     @Override
     public void onBackPressed() {
@@ -141,40 +111,6 @@ public void changewifi(View view){
         Toast.makeText(MainActivity.this, "Major Social Apps has been closed", Toast.LENGTH_SHORT).show();
     }
 
-    public void saveGeoSetting(View view){
-        useGPS=(CheckBox)findViewById(R.id.GPScheckbox);
-        optimizeAsILeave=(CheckBox)findViewById(R.id.leaveoptimize);
-        leaveNetOn=(CheckBox)findViewById(R.id.networkCheckbox);
-        SharedPreferences.Editor editor=sharedPreferences.edit();
-        if(useGPS.isChecked() == true) {
-
-            editor.putString("useGPS","active");
-            editor.commit();
-            useGPStest = sharedPreferences.getString("useGPS",null);
-        }
-        else{
-            sharedPreferences.edit().remove("useGPS").commit();
-            useGPStest=null;
-        }
-        if(optimizeAsILeave.isChecked() == true) {
-            editor.putString("optimizeAsILeave", "active");
-            editor.commit();
-            optimizeAsILeavetest = sharedPreferences.getString("optimizeAsILeave",null);
-        }
-        else{
-            sharedPreferences.edit().remove("optimizeAsILeave").commit();
-            optimizeAsILeavetest=null;
-        }
-        if(leaveNetOn.isChecked() == true) {
-            editor.putString("leaveNetOn","active");
-            editor.commit();
-            leaveNetOntest = sharedPreferences.getString("leaveNetOn",null);
-        }
-        else{
-            sharedPreferences.edit().remove("leaveNetOn").commit();
-            leaveNetOntest=null;
-        }
-    }
 
     public void saveSettingPref(View view){
 
@@ -182,17 +118,21 @@ public void changewifi(View view){
         screenOffKill=(CheckBox)findViewById(R.id.screenOffKill);
         SharedPreferences.Editor editor=sharedPreferences.edit();
         if(backgroundCheck.isChecked() == true) {
-            editor.putString("backgroundworking","active");
+            editor.putString("backgroundworking", "active");
             editor.commit();
+            background = sharedPreferences.getString("backgroundworking", null);
         }
         else{
+            background = null;
             sharedPreferences.edit().remove("backgroundworking").commit();
         }
         if(screenOffKill.isChecked()==true){
             editor.putString("screenoffkill", "active");
             editor.commit();
+            screenKill=sharedPreferences.getString("screenoffkill", null);
         }
         else{
+            screenKill=null;
             sharedPreferences.edit().remove("screenoffkill").commit();
         }
 
@@ -276,10 +216,6 @@ public void changewifi(View view){
             openFragment(new profile_fragment());
         } else if (id == R.id.optimization) {
             openFragment(new BlankFragment());
-        } else if (id == R.id.geosetting) {
-
-            openFragment(new GeoSettingFragement());
-
         } else if (id == R.id.setting) {
 
             openFragment(new SettingFragment());
@@ -295,6 +231,4 @@ public void changewifi(View view){
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
 }
